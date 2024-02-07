8e71c846d1403c0f75dd1560c364ef802dfc9fcc
Roshan vaidya <roshanvaidya1212002@gmailcom>
2024-02-07 11:27:18 +0530

    Updated main file

---
+++ main.py
@@ -0,0 +1,41 @@
+from student import Student
+from datetime import date
+from course import Course
+objstudent=Student()
+objCourse=Course()
+
+def Menu():
+    try:
+        while True:
+            print("Choose any Menu:\n\n\t1. Show all Students \n\t2. Add Student\n\t3. View Batch-Wise Stats\n\t4. Exit\n")
+            menu=int(input("Enter a menu: "))
+            objCourse.define_courses()
+            if menu==1:
+                objCourse.get_python_batch()
+                print()
+                objCourse.get_da_batch()
+                print()
+                objCourse.get_ds_batch()
+                
+            elif menu==2:
+                name=input("Enter name of student: ")
+                mobile=int(input("Enter mobile number of student: "))
+                address=input("Enter address of student: ")
+                course_name=input("Enter a course name: ")
+                joining_date=date.today()
+                
+                objstudent.Addstudent(name,mobile,address,course_name,joining_date) 
+                
+            elif menu==3:
+                
+               objCourse.show_batch_wise_stats()
+        
+            elif menu==4:
+                break  
+    
+    except Exception as e:
+        print(e)
+        
+Menu()
+        
+          
